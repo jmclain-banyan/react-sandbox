e2965b6d5582d886634db869395054966f3893ef
Josh McLain <joshmclain45@gmail.com>
2021-05-10 10:45:32 -0700

    made header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>If you're reading this, then it worked.</h1>
+        </header>
+    )
+}
+
+export default Header
